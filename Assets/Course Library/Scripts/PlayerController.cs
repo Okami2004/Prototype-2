1bc380afc23b9703c771ea6ecebc80bbc4f122d1
Logan-Okami <51331444+Okami2004@users.noreply.github.com>
2019-12-02 10:17:42 -0800

    almost done with 2.2

--- Assets/Course Library/Scripts/PlayerController.cs
+++ Assets/Course Library/Scripts/PlayerController.cs
@@ -5,7 +5,11 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float horizontalInput;
-    public float speed = 10f;
+    public float speed = 10.0f;
+    public float leftboundary = -17.0f;
+    public float rightboundary = 17.0f;
+    public GameObject projectilePrefab; 
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,12 +19,25 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update() 
     {
-        if (transform.postition.x < -10)
+        if (transform.position.x < leftboundary)
         {
-            transform.position = new Vector3(-10, transform.postion.y, transform.postion.z);
+            transform.position = new Vector3(rightboundary, transform.position.y, transform.position.z);
         }
-    }
+          if (transform.position.x > rightboundary)
+        {
+            transform.position = new Vector3(leftboundary, transform.position.y, transform.position.z);
+        }
+    
         horizontalInput = Input.GetAxis("Horizontal");
-        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime *speed);
-    }
-}
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        } else if (Input.GetKeyUp(KeyCode.Space))
+        {
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
+
+    } 
+}       
